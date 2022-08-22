9449c9736c7fc513eb9dac511420391723553df0
AndreVale69 <sforzini31@gmail.com>
2022-08-22 20:55:13 +0200

    Add input information (height only)

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# test 1
-# test 2
+# Insert all informations of automatic warehouse
+print("Insert all informations of automatic warehouse below\n\n")
+
+height_left = input("Height left column (meters)          : ")
+
+height_right_upper = input("Height right column - Upper (meters) : ")
+
+height_right_lower = input("Height right column - Lower (meters) : ")
