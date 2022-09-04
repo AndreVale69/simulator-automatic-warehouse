6381dd00d00fd77c98edf97358b1624390e16858
AndreVale69 <sforzini31@gmail.com>
2022-09-04 19:40:06 +0200

    Add same height check

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ data_left_column = np.array([], dtype=type)
 data_right_column = np.array([], dtype=type)
 
 # Insert all information of automatic warehouse
-print("Enter all information of automatic warehouse below\n\n")
+print("\nEnter all information of automatic warehouse below\n\n")
 
 # Create a automatic warehouse with default height, otherwise creare with custom height
-# TODO the height of left column and the height of right column must be the same!
 if int(input("Debug mode? [Yes = 1 / No = 0]: ")) == 1:
     data_left_column = debug.create_left_column(data_left_column)
+    height_left_column = ware_op.height_left_column(data_left_column)
+
     data_right_column = debug.create_right_column(data_right_column)
     for i in range(data_right_column.size):
         if data_right_column[i][0] == "Storage":
@@ -27,20 +28,39 @@ if int(input("Debug mode? [Yes = 1 / No = 0]: ")) == 1:
                 if data_right_column[i][0] == "Hole":
                     hole = data_right_column[i][1]
 else:
-    print("Create left column...\n")
-    data_left_column = ware_op.column_construction(data_left_column)
+    is_valid = 0
+    while is_valid == 0:
+        print("Create left column...\n")
+        data_left_column = ware_op.column_construction(data_left_column)
 
-    print("\n\nCreate right column...")
-    data_right_column = ware_op.column_construction(data_right_column)
+        height_left_column = ware_op.height_left_column(data_left_column)
+        print("\nAttention! The height of left column is: " + str(height_left_column) + "\nThe height of right column must be the same.")
 
-    storage_area = int(input("Insert height storage area: "))
-    data_right_column = np.insert(data_right_column, data_right_column.size, ("Storage", storage_area))
+        buffer_area = int(input("Insert height buffer area: "))
+        data_right_column = np.insert(data_right_column, data_right_column.size, ("Buffer", buffer_area))
 
-    buffer_area = int(input("Insert height buffer area: "))
-    data_right_column = np.insert(data_right_column, data_right_column.size, ("Buffer", buffer_area))
+        storage_area = int(input("Insert height storage area: "))
+        data_right_column = np.insert(data_right_column, data_right_column.size, ("Storage", storage_area))
 
-    hole = int(input("Insert height right hole: "))
-    data_right_column = np.insert(data_right_column, data_right_column.size, ("Hole", hole))
+        hole = int(input("Insert height right hole: "))
+        data_right_column = np.insert(data_right_column, data_right_column.size, ("Hole", hole))
+
+        tmp_height_right = ware_op.height_right_column(data_right_column, storage_area, buffer_area, hole)
+        
+        if tmp_height_right > height_left_column:
+            print("\n!ERROR!\nThe height of right column (" + str(tmp_height_right) + ") is greater than the height of left column (" + str(height_left_column) + ")\n")
+            break
+        else:
+            if tmp_height_right == height_left_column:
+                print("\n!ERROR!\nThe height of left column (" + str(height_left_column) + ") is the same of the height of right column (" + str(tmp_height_right) + ")\n")
+                break
+        
+        print("\nCurrent right height: " + str(tmp_height_right) + "\nLeft height: " + str(height_left_column))
+
+        print("\nCreate right column...")
+        data_right_column = ware_op.column_construction(data_right_column, data_left_column, True)
+
+        is_valid = 1
 
 # Check arrays
 print(data_left_column)
@@ -57,7 +77,6 @@ print("Numbers drawers right: " + str(numbers_drawer_right))
 print("Numbers space left   : " + str(numbers_space_left))
 print("Numbers space right  : " + str(numbers_space_right))
 
-height_left_column = ware_op.height_left_column(data_left_column)
 height_right_column = ware_op.height_right_column(data_right_column, storage_area, buffer_area, hole)
 height_warehouse = height_left_column if height_left_column > height_right_column else height_right_column
 
--- warehouse_operations.py
+++ warehouse_operations.py
@@ -1,3 +1,4 @@
+from subprocess import check_output
 import useful_functions as us_fun
 import numpy as np
 
@@ -6,16 +7,19 @@ __height_btw_drawers = 25
 
 
 # Build a column of warehouse
-def column_construction(data_column: np.ndarray):
+def column_construction(data_column: np.ndarray, to_compare_data: np.ndarray = np.array([]), is_second: bool = False):
     escape = False
     index = 0
     while escape == False:
         choice = int(input("\nAttention: Don't consider the spaces near the drawers\n"
+                        "The current height is: " + str(algorithm_calculate_height(data_column)) + "\n"
                         "Select:\n"
                         "1) Enter 1 empty space (25/space);\n"
                         "2) Enter (input) empty space (25/space);\n"
                         "3) Enter height 1 drawer.\n"
+                        "4) Exit.\n"
                         "Choice: "))
+
         if choice == 1:
             data_column = np.insert(data_column, index, ("Space", __height_btw_drawers))
             index += 1
@@ -37,7 +41,23 @@ def column_construction(data_column: np.ndarray):
                     data_column = np.insert(data_column, index, ("Drawer", height_drawer))
                     index += 1
                 else:
+                    if choice == 4:
+                        answer = str(input("All information will be lost. Are you sure? [Y/N]: "))
+                        if answer  != "N":
+                            exit(0)
+                        
+                    
                     print("ERROR!!! Enter 1, 2 or 3!")
+
+        if is_second:
+            res_compare = compare_to_height(data_column, to_compare_data)
+            if res_compare == 1:
+                raise Exception("The height of right column is grater than left column.")
+            else:
+                if res_compare == 0:
+                    print("Attention! The height of left column is the same of right column")
+
+        print("Now the height is: " + str(algorithm_calculate_height(data_column)))
 
         answer = str(input("Continue? [Y/N] "))
         escape = us_fun.check_answer(answer)
@@ -93,3 +113,24 @@ def algorithm_calculate_height(data_column: np.ndarray, height_column: int = 0):
                 height_column += __height_btw_drawers + data_column[i][1]
 
     return height_column
+
+
+def compare_to_height(data_column_1: np.ndarray, data_column_2: np.ndarray):
+    longer_length = data_column_1.size if data_column_1.size > data_column_2.size else data_column_2.size
+
+    heigth_left = 0
+    heigth_right = 0
+
+    for i in range(longer_length):
+        if i < data_column_1.size:
+            heigth_left += data_column_1[i][1]
+        if i < data_column_2.size:
+            heigth_right += data_column_2[i][1]
+    
+    if heigth_left > heigth_right:
+        return 1
+    else:
+        if heigth_left < heigth_right:
+            return -1
+        else:
+            return 0
