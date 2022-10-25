0915d5de7a58fb15c0a6ac64a46628a0b1e0abde
AndreVale69 <sforzini31@gmail.com>
2022-10-25 20:21:12 +0200

    temp function to calculate space request

--- src/warehouse.py
+++ src/warehouse.py
@@ -7,9 +7,20 @@ class Warehouse(object):
     # __warehouse = np.array([], dtype=__type)
     __warehouse = []
     __height = 0
+    __minimum_height = 500
 
-    def __init__(self, height: int = 0):
+    def __init__(self, height: int):
         self.__height = height
+
+        # error
+        if height < self.__minimum_height:
+            raise ValueError("The height is too low, please digit a value grater/equal than " +
+                             str(self.__minimum_height))
+
+        # creation of space
+        num_space = height // 25
+        for i in range(num_space):
+            self.__warehouse.append(25)
 
     def set_height(self, height: int):
         self.__height = height
@@ -21,9 +32,30 @@ class Warehouse(object):
         return self.__warehouse
 
     def add_drawer(self, drawer: Drawer):
+        self.__count_minimum_space(drawer.get_max_height())
         self.__warehouse.append(drawer)
+
+    # TODO: to finish
+    def __count_minimum_space(self, request):
+        if (request % 25) == 0:
+            effective_req = request // 25
+        else:
+            effective_req = (request // 25) + 1
+
+        min = len(self.__warehouse)
+        count = 0
+
+        for i in range(len(self.__warehouse) - 1):
+            if (self.__warehouse[i] == 25) & (self.__warehouse[i + 1] == 25):
+                count = count + 1
+            else:
+                if count < min:
+                    min = count
+                    index = i - count
+
+                count = 0
+
 
     # TODO: debug
     def print_warehouse(self):
         print(self.__warehouse)
-
