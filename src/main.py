29eb341dc1bc137e67c5c1e7eaa0184de6e457b0
AndreVale69 <sforzini31@gmail.com>
2022-10-27 02:05:20 +0200

    add double column and ready to introduction

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from src.material import Material
 # create warehouse
 warehouse = Warehouse(500)
 
-material = Material(123, "name", 256, 789, 12345, warehouse.get_height())
-material2 = Material(234, "abc", 126, 987, 00000, warehouse.get_height())
+material = Material(123, "name", 256, 789, 12345)
+material2 = Material(234, "abc", 126, 987, 00000)
 drawer = Drawer([material, material2])
 
-material3 = Material(567, "def", 128, 564, 0, warehouse.get_height())
+material3 = Material(567, "def", 128, 564, 0)
 drawer2 = Drawer([material3])
 
-material4 = Material(890, "ghi", 75, 564, 0, warehouse.get_height())
+material4 = Material(890, "ghi", 75, 564, 0)
 # Trigger error with this line:
 # material4 = Material(890, "ghi", 76, 564, 0, warehouse.get_height())
 drawer3 = Drawer([material4])
@@ -63,21 +63,27 @@ warehouse.print_warehouse()
 print()
 
 print("Add new drawer (25 * 3): ")
-warehouse.add_drawer(Drawer([Material(147, "lmn", 75, 637, 0, warehouse.get_height())]))
+warehouse.add_drawer(Drawer([Material(147, "lmn", 75, 637, 0)]))
 warehouse.print_warehouse()
 print()
 
 print("Add new drawer (25 * 2): ")
-warehouse.add_drawer(Drawer([Material(258, "opq", 50, 103, 0, warehouse.get_height())]))
+warehouse.add_drawer(Drawer([Material(258, "opq", 50, 103, 0)]))
 warehouse.print_warehouse()
 print()
 
-# print("Add new drawer (25 * 5, error!): ")
-# warehouse.add_drawer(Drawer([Material(369, "rst", 125, 107, 0, warehouse.get_height())]))
+drawer7 = Drawer([Material(369, "rst", 125, 107, 0)])
+print("Add new drawer (25 * 5, change column!): ")
+warehouse.add_drawer(drawer7)
+warehouse.print_warehouse()
+print()
+
+print("Remove drawer of right column: ")
+warehouse.remove_drawer(drawer7)
+warehouse.print_warehouse()
+print()
+
+# print("Add new drawer (25 * 1): ")
+# warehouse.add_drawer(Drawer([Material(753, "uvz", 17, 346, 0, warehouse.get_height())]))
 # warehouse.print_warehouse()
 # print()
-
-print("Add new drawer (25 * 1): ")
-warehouse.add_drawer(Drawer([Material(753, "uvz", 17, 346, 0, warehouse.get_height())]))
-warehouse.print_warehouse()
-print()
