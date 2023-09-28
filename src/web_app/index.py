5a70cbbca315b4b9c04b3218fc7b032f424505c9
AndreVale69 <sforzini31@gmail.com>
2023-09-28 08:52:49 +0200

    tmp commit backup

--- src/web_app/index.py
+++ src/web_app/index.py
@@ -2,22 +2,26 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, dcc, html, Input, Output, ctx
+from simpy import Store
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
-import datetime
+from pandas import Series, Timestamp
+from datetime import datetime, timedelta
 
 from src.sim.warehouse import Warehouse
 from collections import Counter
 
+
+"""
+    #####################
+    * Set-up simulation *
+    #####################
+"""
 # Initialize the Warehouse for simulation
 warehouse = Warehouse()
 warehouse.run_simulation()
 cn = Counter(warehouse.get_events_to_simulate())
-
-# Import bootstrap components
-BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-app = Dash(external_stylesheets=[BS, dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 
 """
@@ -25,14 +29,18 @@ app = Dash(external_stylesheets=[BS, dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
     * Set-up all components *
     #########################
 """
-# see https://plotly.com/python/px-arguments/ for more options
+# Import bootstrap components
+BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+app = Dash(external_stylesheets=[BS, dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
+
+# See https://plotly.com/python/px-arguments/ for more options
 # df = pd.DataFrame({
 #     "Actions": ["Send back a drawer", "Extract a drawer", "Insert material", "Remove material"],
 #     "Nums Executions": [cn.get("send_back"), cn.get("extract_drawer"), cn.get("ins_mat"), cn.get("rmv_mat")]
 # })
 
-#fig = px.bar(df, x="Actions", y="Nums Executions", barmode="stack",
-#             title='Number of actions performed', color='Actions')
+# fig = px.bar(df, x="Actions", y="Nums Executions", barmode="stack",
+#              title='Number of actions performed', color='Actions')
 
 #prova = [
 #    dict(Action="Job A", Start='2009-01-01', Finish='2009-02-28'),
@@ -41,26 +49,36 @@ app = Dash(external_stylesheets=[BS, dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 #]
 #prova.append(dict(Action="Job D", Start='2009-02-20', Finish='2009-05-30'))
 
-store_obj = warehouse.get_simulation().get_store_history()
-history = []
-for info in range(store_obj.capacity):
+# Get the simulation history
+store_obj: Store = warehouse.get_simulation().get_store_history()
+history = list()
+# Create a list of labels, so take only the Action name from the object "store_obj"
+for index in range(store_obj.capacity):
     history.append(store_obj.get().value)
+
 
 df = pd.DataFrame(history)
 
-from pandas import Series
-prova:Series = df.min()
-prova2:Series = df.max()
-prova3:datetime = datetime.datetime.now()
+# Take the minimum and maximum time of the simulation
+min_time_sim: Timestamp = df.min().get('Start')
+max_time_sim: Timestamp = df.max().get('Finish')
+prova1 = min_time_sim.time()
+prova2 = max_time_sim.time()
+prova3 = min_time_sim.date()
+prova4 = max_time_sim.date()
+diff_days: timedelta = prova4 - prova3
+diff_hours = (prova2 - prova1) # See more: https://stackoverflow.com/questions/25265379/how-do-you-get-the-difference-between-two-time-objects-in-python
 
+# Create the timeline
 fig = px.timeline(df,
                   x_start="Start", x_end="Finish", y="Action",
-                  range_x=[prova.get('Start'), prova2.get('Finish')])
+                  range_x=[min_time_sim, max_time_sim])
 fig.update_yaxes(autorange="reversed") # otherwise, tasks are listed from the bottom up
-# if you want a linear timeline:
+# If you want a linear timeline:
 # fig.layout.xaxis.type = 'linear'
 fig.layout.xaxis.type = 'date'
-# fig.update_xaxes(range=[prova.get('Start'), prova2.get('Finish')])
+# If you want a rangeselector to zoom on each section
+# See more: https://plotly.com/python/range-slider/
 # fig.update_xaxes(
 #     rangeslider_visible = True,
 #     rangeselector = dict(
@@ -82,47 +100,37 @@ fig.layout.xaxis.type = 'date'
 app.layout = html.Div(children=[
     html.H1(
         children='Warehouse simulator',
-        style={
-            'textAlign': 'center',
-        }
+        style={'textAlign': 'center'}
     ),
-
-    # html.Div(children='How many actions have been performed.', style={
-    #     'textAlign': 'center',
-    # }),
 
     dcc.Graph(
         id='graph-actions',
         figure=fig
     ),
 
-    dbc.DropdownMenu(
-        [
+    dbc.DropdownMenu(children=[
             dbc.DropdownMenuItem(
-                [html.I(className='bi bi-download'), " SVG"], id="dropdown-btn_svg", n_clicks=0
+                children=[html.I(className='bi bi-download'), " SVG"],
+                id="dropdown-btn_svg",
+                n_clicks=0
             ),
             dbc.DropdownMenuItem(
-                [html.I(className='bi bi-download'), " PDF"], id="dropdown-btn_pdf", n_clicks=0
-            )
-        ],
+                children=[html.I(className='bi bi-download'), " PDF"],
+                id="dropdown-btn_pdf",
+                n_clicks=0
+            )],
         label="Download Graph"
     ),
 
     dbc.Button([html.I(className='bi bi-arrow-right-circle-fill')], id='btn', n_clicks=0)
-    #dcc.Download(id="download-svg"),
-    #dcc.Download(id="download-pdf")
-
-    # html.Div([
-    #     dbc.Button("Download Graph as SVG", id="btn_svg", color="primary"),
-    #     dcc.Download(id="download-svg"),
-
-    #     dbc.Button("Download Graph as PDF", id="btn_pdf"),
-    #     dcc.Download(id="download-pdf")
-    # ], className="d-grid gap-2 col-6 mx-auto")
-
 ])
 
 
+""" 
+    ##########################
+    * Application's callback * 
+    ##########################
+"""
 @app.callback(
     Output("download-svg", "data"),
     Input("dropdown-btn_svg", "n_clicks"),
@@ -148,7 +156,7 @@ def download_graph(b_svg, b_pdf):
     prevent_initial_call=True
 )
 def update_graph(n_clicks):
-    return fig.update_xaxes(range=[prova.get('Start'), prova3])
+    return fig.update_xaxes(range=[min_time_sim.get('Start'), max_time_sim.get('Finish')])
 
 
 if __name__ == '__main__':
