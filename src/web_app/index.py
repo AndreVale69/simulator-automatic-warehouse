c2b7356a029541fc64c820447e3197edac8089ac
AndreVale69 <sforzini31@gmail.com>
2023-11-01 01:11:23 +0100

    Added form check and run simulation beta

--- src/web_app/index.py
+++ src/web_app/index.py
@@ -29,7 +29,6 @@ warehouse = Warehouse()
 warehouse.run_simulation()
 cn = Counter(warehouse.get_events_to_simulate())
 
-
 """
     #########################
     * Set-up all components *
@@ -125,22 +124,47 @@ def serve_layout():
         dbc.Row([
             dbc.Col([
                 dbc.Card([
-                    dbc.CardHeader(children=html.H4("Timeline Utilities", className="card-title")),
+                    dbc.CardHeader(children=html.H4("New Simulation", className="card-title")),
 
-                    dbc.CardBody(
-                        [
-                            dbc.InputGroup([
-                                # info text
-                                dbc.InputGroupText(children='View range:'),
-                                # input text
-                                dbc.Input(id='set_step_graph', type='number', min=1, value=timeline.get_step(),
-                                          max=timeline.get_tot_tabs(), step=1),
-                                dbc.InputGroupText(children='min.')
+                    dbc.CardBody([
+                        dbc.ListGroup([
+                            dbc.ListGroupItem([
+                                dbc.Label('Total number of actions:'),
+                                dbc.Input(id='num_actions_sim', type='number', min=1, step=1, placeholder='# Actions', required=True) # TODO: add '?' and write in what does it means. You can use also 'info' icon (boostrap)
+                            ]),
+                            dbc.ListGroupItem([
+                                dbc.Label('Total number of drawers:'),
+                                dbc.Input(id='num_drawers_sim', type='number', min=1, step=1, placeholder='# Drawers')
+                            ]),
+                            dbc.ListGroupItem([
+                                dbc.Label('Total number of materials:'),
+                                dbc.Input(id='num_materials_sim', type='number', min=1, step=1, placeholder='# Materials')
+                            ]),
+                            dbc.ListGroupItem([
+                                dbc.Label('Do you want fill carousel?'),
+                                dbc.Checklist(
+                                    options=[
+                                        {"value": "gen_deposit", "label": "Generate deposit drawer"},
+                                        {"value": "gen_buffer", "label": "Generate buffer drawer"},
+                                    ],
+                                    id="checklist_generators",
+                                )
+                            ]),
+                            dbc.ListGroupItem([
+                                dbc.Label('Total time of the simulation:'),
+                                dbc.InputGroup([
+                                    dbc.InputGroupText(dbc.Checkbox(id='checkbox_time_sim', value=False)),
+                                    dbc.Input(id='time_sim', type='number', min=1, step=1, placeholder='Seconds', readonly=True)
+                                ]),
                             ])
-                        ]
-                    ),
+                        ])
+                    ]),
 
-                    dbc.CardFooter("This is the footer"),
+                    dbc.CardFooter([
+                        dbc.Col([
+                            dbc.Button(children='Run new simulation!', id='btn_new_simulation', n_clicks=0)
+                        ], width={'offset': 2})
+                    ]),
                 ])
             ]),
             dbc.Col([
@@ -150,6 +174,19 @@ def serve_layout():
                     dbc.CardBody(
                         [
                             dbc.Row([
+                                dbc.Col([
+                                    dbc.Col([
+                                        dbc.InputGroup([
+                                            # info text
+                                            dbc.InputGroupText(children='View range:'),
+                                            # input text
+                                            dbc.Input(id='set_step_graph', type='number', min=1,
+                                                      value=timeline.get_step(),
+                                                      max=timeline.get_tot_tabs(), step=1),
+                                            dbc.InputGroupText(children='min.')
+                                        ])
+                                    ])
+                                ], width='auto'),
                                 dbc.Col([
                                     dbc.InputGroup([
                                         # go to extreme left
@@ -162,7 +199,7 @@ def serve_layout():
                                         dbc.Input(id='actual_tab', type='number', min=1, max=timeline.get_tot_tabs(),
                                                   step=1,
                                                   value=timeline.get_actual_tab()),
-                                        dbc.InputGroupText(id='num_tabs_graph', children=f'/{timeline.get_tot_tabs()}'), # TODO: fix its value when goes to the limit
+                                            dbc.InputGroupText(id='num_tabs_graph', children=f'/{timeline.get_tot_tabs()}'), # TODO: fix its value when goes to the limit
                                         # go to right
                                         dbc.Button([html.I(className='bi bi-chevron-right')], id='btn_right',
                                                    n_clicks=0),
@@ -170,7 +207,7 @@ def serve_layout():
                                         dbc.Button([html.I(className='bi bi-chevron-bar-right')], id='btn_right_end',
                                                    n_clicks=0)
                                     ])
-                                ], width={"size": 'auto', "offset": 4}),
+                                ], width={"size": 'auto', "offset": 2}),
                                 dbc.Col([
                                     # summary
                                     dbc.Button([html.I(className='bi bi-x-circle-fill'), " SUMMARY"], id='btn_summary',
@@ -258,8 +295,8 @@ def download_graph(b_svg, b_pdf):
     State('set_step_graph', 'invalid'),
     prevent_initial_call=True
 )
-def update_components(val_btn_right, val_btn_left, val_btn_right_end, val_btn_left_end, val_btn_summary,
-                      val_actual_tab, val_set_step_graph, state_set_step_graph):
+def update_timeline_components(val_btn_right, val_btn_left, val_btn_right_end, val_btn_left_end, val_btn_summary,
+                               val_actual_tab, val_set_step_graph, state_set_step_graph):
     # Use switch case because is more efficiently than if-else
     # Source: https://www.geeksforgeeks.org/switch-vs-else/
     invalid_actual_tab: bool = True if val_actual_tab is None else False
@@ -317,6 +354,148 @@ def update_components(val_btn_right, val_btn_left, val_btn_right_end, val_btn_le
             invalid_actual_tab,
             f'/{timeline.get_tot_tabs()}',
             state_set_step_graph)
+
+
+@app.callback(
+    Output('num_actions_sim', 'value'),
+    Output('num_drawers_sim', 'value'),
+    Output('num_materials_sim', 'value'),
+    Output('num_actions_sim', 'invalid'),
+    Output('num_drawers_sim', 'invalid'),
+    Output('num_materials_sim', 'invalid'),
+    Output('time_sim', 'readonly'),
+    Output('time_sim', 'value'),
+
+
+    Input('num_actions_sim', 'value'),
+    Input('num_drawers_sim', 'value'),
+    Input('num_materials_sim', 'value'),
+    Input('checklist_generators', 'value'),
+    Input('checkbox_time_sim', 'value'),
+
+
+    State('num_actions_sim', 'value'),
+    State('num_drawers_sim', 'value'),
+    State('num_materials_sim', 'value'),
+    State('num_actions_sim', 'invalid'),
+    State('num_drawers_sim', 'invalid'),
+    State('num_materials_sim', 'invalid'),
+    State('time_sim', 'readonly'),
+    State('checkbox_time_sim', 'value'),
+    State('checklist_generators', 'options'),
+)
+def update_new_sim_components(
+        # Inputs:
+        actions_val, drawers_val, materials_val, checklist_generators_val, checkbox_time_val,
+        # States:
+        actions_val_state, drawers_val_state, materials_val_state, actions_invalid_state, drawers_invalid_state,
+        materials_invalid_state, checkbox_time_readonly, checkbox_time_value, checklist_generators_options):
+    match ctx.triggered_id:
+        case 'num_actions_sim':
+            actions_invalid_state = True if actions_val is None else False
+
+        case 'num_drawers_sim':
+            drawers_invalid_state = True if drawers_val is None else False
+
+        case 'num_materials_sim':
+            materials_invalid_state = True if materials_val is None else False
+
+        case 'checkbox_time_sim':
+            checkbox_time_readonly = not checkbox_time_val
+            checkbox_time_val = None
+
+        # TODO: to rmv
+        case 'checklist_generators':
+            return (actions_val_state,
+                    drawers_val_state,
+                    materials_val_state,
+                    actions_invalid_state,
+                    drawers_invalid_state,
+                    materials_invalid_state,
+                    checkbox_time_readonly,
+                    checkbox_time_val)
+
+    return (actions_val_state,
+            drawers_val_state,
+            materials_val_state,
+            actions_invalid_state,
+            drawers_invalid_state,
+            materials_invalid_state,
+            checkbox_time_readonly,
+            checkbox_time_val)
+
+
+@app.callback(
+    Output('time_sim', 'invalid'),
+    Input('time_sim', 'value'),
+    State('time_sim', 'readonly')
+)
+def update_time_sim_input(time_sim_val, time_sim_readonly):
+    if not time_sim_readonly:
+        return True if time_sim_val is None else False
+
+
+# TODO: DEBUG!
+@app.callback(
+    Output('graph-actions', '_chart_types'),
+
+    Input('btn_new_simulation', 'n_clicks'),
+
+    State('num_actions_sim', 'value'), # TODO: to check
+    State('num_drawers_sim', 'value'), # TODO: to check
+    State('num_materials_sim', 'value'), # TODO: to check
+    State('checklist_generators', 'value'), # TODO: opt
+    State('checkbox_time_sim', 'value'), # TODO: opt
+    State('time_sim', 'value'), # TODO: it depends from checkbox_time_sim
+    State('graph-actions', 'figure')
+)
+def exec_new_simulation(clicks_btn, num_actions_sim, num_drawers_sim, num_materials_sim, checklist_generators, checkbox_time_sim, time_sim, timeline):
+    actions_invalid = True if num_actions_sim is None else False
+    drawers_invalid = True if num_drawers_sim is None else False
+    materials_invalid = True if num_materials_sim is None else False
+    checklist_generators = dict() if checklist_generators is None else checklist_generators
+    time_sim_invalid = False
+    if checkbox_time_sim:
+        time_sim_invalid = True if time_sim is None else False
+
+    # run new simulation if there are no probs
+    if not actions_invalid and not drawers_invalid and not materials_invalid and not time_sim_invalid:
+        warehouse.new_simulation(num_actions=num_actions_sim,
+                                 num_gen_drawers=num_drawers_sim,
+                                 num_gen_materials=num_materials_sim,
+                                 gen_deposit=True if 'gen_deposit' in checklist_generators else False,
+                                 gen_buffer=True if 'gen_buffer' in checklist_generators else False,
+                                 time=time_sim)
+
+        store_obj: Store = warehouse.get_simulation().get_store_history()
+        history = list()
+        # Create a list of labels, so take only the Action name from the object "store_obj"
+        for index in range(store_obj.capacity):
+            history.append(store_obj.get().value)
+
+        df = pd.DataFrame(history)
+
+        # Take the minimum and maximum time of the simulation
+        min_time_sim: Timestamp = df.min().get('Start')
+        max_time_sim: Timestamp = df.max().get('Finish')
+        timeline = Timeline(min_time_sim, max_time_sim)
+
+        # Create the timeline
+        fig = px.timeline(df,
+                          x_start="Start", x_end="Finish", y="Action",
+                          range_x=[timeline.get_actual_left(), timeline.get_actual_right()],
+                          color="Action")
+        fig.update_yaxes(autorange="reversed")  # otherwise, tasks are listed from the bottom up
+        # If you want a linear timeline:
+        # fig.layout.xaxis.type = 'linear'
+        fig.layout.xaxis.type = 'date'
+        # create slider
+        fig.update_xaxes(rangeslider=dict(visible=True, range=[min_time_sim, max_time_sim]))
+
+        return fig
+
+    # report errors
+    return timeline
 
 
 if __name__ == '__main__':
