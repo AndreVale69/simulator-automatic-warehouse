e52980236035b39d2e90e7b0aa2f918cb2030b25
AndreVale69 <sforzini31@gmail.com>
2024-03-02 13:39:54 +0100

    Updated main

--- src/sim/__main__.py
+++ src/sim/__main__.py
@@ -1,32 +1,18 @@
 from sim.warehouse import Warehouse
-from sim.utils.save_warehouse_state import save_config
-import random
 
-
-def work(sad: int = None):
-    if sad is not None:
-        random.seed(sad)
-
-    # gen Warehouse
-    warehouse = Warehouse()
-
-    # run simulation
-    warehouse.run_simulation()
-
-    # save the configuration at the end
-    save_config(warehouse.get_simulation().get_warehouse())
 
 if __name__ == "__main__":
-    work()
-    # to find any error - brutal force
-    # seed: int = 1000
-    # while seed <= 10_000:
-    #     try:
-    #         work(seed)
-    #     except Exception as e:
-    #         print(f'SEED: {seed}')
-    #         exit(1)
-    #     seed += 1
+    # debug purpose
+    # import random
+    # random.seed(None)
+
+    # gen warehouse and run simulation
+    (warehouse := Warehouse()).run_simulation()
+
+    # save the configuration at the end
+    # from sim.utils.save_warehouse_state import save_config
+    # save_config(warehouse.get_simulation().get_warehouse())
+
 
 # OLD ERRORS:
 # AttributeError: 'EmptyEntry' object has no attribute 'get_drawer'
