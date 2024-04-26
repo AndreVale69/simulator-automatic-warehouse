ea9ae637811498577636b60faafdd09ddaf86744
AndreVale69 <sforzini31@gmail.com>
2024-04-26 15:10:27 +0200

    Added comments in move.py

--- src/sim/status_warehouse/simulate_events/move/move.py
+++ src/sim/status_warehouse/simulate_events/move/move.py
@@ -9,18 +9,51 @@ from src.sim.warehouse import Warehouse  # , Drawer
 
 
 class Move(Action):
-    def __init__(self, env: Environment, warehouse: Warehouse, simulation: Simulation, destination: EnumWarehouse, drawer=None):
+    def __init__(self, env: Environment, warehouse: Warehouse, simulation: Simulation, destination: EnumWarehouse,
+                 drawer=None):
+        """
+        Superclass for all simple moves.
+
+        :type env: Environment
+        :type warehouse: Warehouse
+        :type simulation: Simulation
+        :type destination: EnumWarehouse
+        :type drawer: Drawer
+        :param env: the simulation environment (SimPy Environment).
+        :param warehouse: the warehouse where the action is performed.
+        :param simulation: the simulation where the action is performed.
+        :param destination: the destination of the move.
+        :param drawer: the drawer used in the movement.
+        """
         super().__init__(env, warehouse, simulation)
         self.drawer = drawer
         self.destination = destination
 
     def set_drawer(self, drawer):
+        """
+        Sets the drawer used in the movement.
+
+        :type drawer: Drawer
+        :param drawer: drawer used in the movement.
+        """
         self.drawer = drawer
 
     def get_drawer(self):
+        """
+        Get the drawer used in the movement.
+
+        :rtype: Drawer
+        :return: the drawer used in the movement.
+        """
         return self.drawer
 
-    def get_destination(self):
+    def get_destination(self) -> EnumWarehouse:
+        """
+        Get the destination of the move.
+
+        :rtype: EnumWarehouse
+        :return: the destination of the move.
+        """
         return self.destination
 
     @abstractmethod
