1b5badffb9dbfc81eadf70ded2e2bf76a3543ddb
AndreVale69 <sforzini31@gmail.com>
2024-05-08 07:07:04 +0200

    Added tests action

---
+++ tests/sim/status_warehouse/simulate_events/test_action.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+from simpy import Environment
+
+from src.sim.warehouse import Warehouse
+from src.sim.status_warehouse.simulate_events.action import Action
+
+
+class TestAction(TestCase):
+    def setUp(self):
+        self.env = Environment()
+        self.warehouse = Warehouse()
+        self.action = Action(self.env, self.warehouse, None)
+
+    def test_get_env(self):
+        # arrange
+        env = self.env
+
+        # act
+        actual_env = self.action.get_env()
+
+        # assert
+        self.assertEqual(actual_env, env)
+
+    def test_get_warehouse(self):
+        # arrange
+        warehouse = self.warehouse
+
+        # act
+        actual_warehouse = self.action.get_warehouse()
+
+        # assert
+        self.assertEqual(actual_warehouse, warehouse)
+
+    def test_get_simulation(self):
+        # arrange
+
+        # act
+        actual_simulation = self.action.get_simulation()
+
+        # assert
+        self.assertIsNone(actual_simulation)
